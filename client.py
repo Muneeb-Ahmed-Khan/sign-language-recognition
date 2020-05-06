8b6f8e141e6b5d5137e6723ac464eaa6baf2e3b7
Muneeb-Ahmed-Khan <49415395+Muneeb-Ahmed-Khan@users.noreply.github.com>
2020-05-06 19:29:51 +0500

    update server.py

--- client.py
+++ client.py
@@ -3,20 +3,21 @@ import requests
 import json
 import cv2
 
-addr = 'http://127.0.0.1:5000'
+addr = 'http://159.65.157.105:5000'
 test_url = addr + '/api/recieveData'
 
 # prepare headers for http request
 content_type = 'image/jpeg'
 headers = {'content-type': content_type}
 
-img = cv2.imread('pic2.jpg')
+img = cv2.imread('image.jpg')
 
 # encode image as jpeg
 _, img_encoded = cv2.imencode('.jpg', img)
-
+print(type(img_encoded.tostring()))
+print(len(img_encoded.tostring()))
 # send http request with image and receive response
-response = requests.post(test_url, data=img_encoded.tostring(), headers=headers)
+response = requests.post(test_url, data=img_encoded.tostring())
 
 # decode response
 print(response.text)
--- server.py
+++ server.py
@@ -13,6 +13,14 @@ import copy
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 
+label_lines = [line.rstrip() for line in tf.gfile.GFile("logs/trained_labels.txt")]
+with tf.gfile.FastGFile("logs/trained_graph.pb", 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    _ = tf.import_graph_def(graph_def, name='')
+
+global_session = tf.Session()
+softmax_tensor = global_session.graph.get_tensor_by_name('final_result:0')
 
 
 # Initialize the Flask application
@@ -21,14 +29,13 @@ app = Flask(__name__)
 # route http posts to this method
 @app.route('/api/recieveData', methods=['POST'])
 def recieveData():
+    
     r = request
     # convert string of image data to uint8
     nparr = np.fromstring(r.data, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     img = cv2.rotate(img, cv2.ROTATE_180)
-
-    #cv2.imshow("IMAGE1", img)
-    #cv2.waitKey(0)
+    img = cv2.flip(img, 1)
 
     #Croping the center 200px square
     height = img.shape[0]
@@ -36,34 +43,27 @@ def recieveData():
     x1, y1, x2, y2 = int((width / 2)  - 100), int((height / 2) - 100), int((width / 2)  + 100), int((height / 2) + 100)
     img_cropped = img[y1:y2, x1:x2]
 
-    #cv2.imshow("IMAGE", img_cropped)
-    #cv2.waitKey(0)
+    # cv2.rectangle(img, (x1, y1), (x2, y2), (255,0,0), 2)
+    # cv2.imshow("IMAGE1", img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
     image_data = cv2.imencode('.jpg', img_cropped)[1].tostring()
     
-    
-    label_lines = [line.rstrip() for line in tf.gfile.GFile("logs/trained_labels.txt")]
-    with tf.gfile.FastGFile("logs/trained_graph.pb", 'rb') as f:
-        graph_def = tf.GraphDef()
-        graph_def.ParseFromString(f.read())
-        _ = tf.import_graph_def(graph_def, name='')
+    predictions = global_session.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})
+    top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
 
-    with tf.Session() as sess:
-        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
-        predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})
-        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
-
-        max_score = 0.0
-        res = ''
-        for node_id in top_k:
-            human_string = label_lines[node_id]
-            score = predictions[0][node_id]
-            print('%s (score = %.5f)' % (human_string, score))
-            if score > max_score:
-                max_score = score
-                res = human_string
+    max_score = 0.0
+    res = ''
+    for node_id in top_k:
+        human_string = label_lines[node_id]
+        score = predictions[0][node_id]
+        #print('%s (score = %.5f)' % (human_string, score))
+        if score > max_score:
+            max_score = score
+            res = human_string
         
     return Response(response= res, status=200, mimetype="application/json")
 
 # start flask app
-app.run(host="0.0.0.0", port=5000)
+app.run(host="159.65.157.105", port=5000)
